2eaed7f8038e2572472a8a133c95bf43516c1bc7
bestDvlpr <khsiddiqov@gmail.com>
2021-02-15 14:44:03 +0500

    filter correction made

--- java/admin/src/main/java/com/exadel/frs/system/swagger/filter/CorsFilter.java
+++ java/admin/src/main/java/com/exadel/frs/system/swagger/filter/CorsFilter.java
@@ -22,7 +22,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Foo, Bar, Baz");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
