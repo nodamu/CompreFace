2b5054fcc10d45dea3d762279a303ec15e6ad581
bestDvlpr <khsiddiqov@gmail.com>
2021-02-15 14:42:00 +0500

    cors config added for dev swagger test

---
+++ java/admin/src/main/java/com/exadel/frs/system/security/config/CorsConfig.java
@@ -0,0 +1,11 @@
+package com.exadel.frs.system.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    
+}
--- java/admin/src/main/java/com/exadel/frs/system/security/config/WebSecurityConfig.java
+++ java/admin/src/main/java/com/exadel/frs/system/security/config/WebSecurityConfig.java
@@ -47,6 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
