e87fe061b4b1ae319efb74557e07b5654e7e1afa
bestDvlpr <khsiddiqov@gmail.com>
2021-02-15 14:48:02 +0500

    config correction made

--- java/admin/src/main/java/com/exadel/frs/system/security/config/CorsConfig.java
+++ java/admin/src/main/java/com/exadel/frs/system/security/config/CorsConfig.java
@@ -7,5 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOrigins("*");
+    }
 }
